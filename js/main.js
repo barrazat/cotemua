de66f69797771361f1e23d2a7763b2df5d242c6b
Leonardo <leobarrazat@gmail.com>
2020-09-25 23:39:20 -0300

    gallery page

--- js/main.js
+++ js/main.js
@@ -4,6 +4,12 @@ selectElement('.mobile-menu').addEventListener('click', () => {
     selectElement('header').classList.toggle('active');
 });
 
+const parallax  = document.getElementById("parallax");
+window.addEventListener("scroll", function(){
+    let offset = window.pageYOffset;
+    parallax.style.backgroundPositionY = offset *0.5 + "px";
+})
+
 function ErrorMessage(){
     var errorEmojis = ["⚠️", "🥺", "😟", "😞", "😣", "😖"];    
     var emojiSelected = Math.floor(Math.random()*errorEmojis.length);
